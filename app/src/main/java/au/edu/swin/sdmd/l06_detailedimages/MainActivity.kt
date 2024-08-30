aa6993a54f372a7b3d15545f2b3dc495dff57aae
N. A. Ronald <nronald@swin.edu.au>
2024-08-30 11:37:53 +1000

    First commit -- no intents

---
+++ app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/MainActivity.kt
@@ -0,0 +1,29 @@
+package au.edu.swin.sdmd.l06_detailedimages
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val station = Location("Glenferrie Station", "Ada May Plante",
+            -37.82154f, 145.03647f)
+
+        val vStation = findViewById<TextView>(R.id.station)
+        vStation.setOnClickListener {
+
+        }
+    }
+}
